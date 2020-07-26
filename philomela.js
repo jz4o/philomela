6767d094e5f56386cdb9573628461342c162f0fc
jz4o <26317360+jz4o@users.noreply.github.com>
2020-07-26 22:46:22 +0900

    implemented function to post message to discord

--- philomela.js
+++ philomela.js
@@ -1,3 +1,18 @@
-function myFunction() {
-  
+const DISCORD = {
+  INCOMING_URL: PropertiesService.getScriptProperties().getProperty('DISCORD_INCOMING_URL')
+};
+
+const test = () => {
+  const message = Browser.inputBox('Please input for post message to Discord');
+  postMessageToDiscord(message);
+}
+
+const postMessageToDiscord = message => {
+  const options = {
+    'method'     : 'post',
+    'contentType': 'application/json',
+    'payload'    : JSON.stringify({ 'content': message })
+  };
+
+  UrlFetchApp.fetch(DISCORD.INCOMING_URL, options);
 }
