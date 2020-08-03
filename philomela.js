755d145779407f261d94dff59e8d26aba5d715b7
jz4o <26317360+jz4o@users.noreply.github.com>
2020-08-04 00:10:48 +0900

    add utilities

--- philomela.js
+++ philomela.js
@@ -68,27 +68,3 @@ const postMessageToDiscord = message => {
 
   UrlFetchApp.fetch(DISCORD.INCOMING_URL, options);
 }
-
-const requestBodyToHash = body => {
-  return body.split('&').reduce((result, parameter) => {
-    const [key, value] = parameter.split('=');
-    result[key] = decodeURIComponent(value);
-
-    return result;
-  }, {});
-}
-
-const shuffle = array => {
-  const result = [];
-  for(i = array.length; i > 0; i--){
-    const index = Math.floor(Math.random() * i);
-    const val = array.splice(index, 1)[0];
-    result.push(val);
-  }
-
-  return result;
-}
-
-const toBoolean = obj => {
-  return String(obj).toLowerCase() === 'true';
-}
---
+++ utilities.js
@@ -0,0 +1,23 @@
+const requestBodyToHash = body => {
+  return body.split('&').reduce((result, parameter) => {
+    const [key, value] = parameter.split('=');
+    result[key] = decodeURIComponent(value);
+
+    return result;
+  }, {});
+}
+
+const shuffle = array => {
+  const result = [];
+  for(i = array.length; i > 0; i--){
+    const index = Math.floor(Math.random() * i);
+    const val = array.splice(index, 1)[0];
+    result.push(val);
+  }
+
+  return result;
+}
+
+const toBoolean = obj => {
+  return String(obj).toLowerCase() === 'true';
+}
